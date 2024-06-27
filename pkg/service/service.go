fca2536daae4a8e233ff7b673ece972c255d1f75
Gilberto Bertin <jibi@cilium.io>
2024-06-27 17:20:19 +0200

    service: differentiate UDP and TCP protocols
    this commit updates the service logic, from the agent all the way down
    to the datapath, to take into account the L4 protocols specified in a
    k8s service.
    
    Historically Cilium has always been ignoring, at least from a end user
    perspective, the protocol specified in a k8s service, resulting in the
    inability to expose a port only for a given protocol, or in the opposite
    case, resulting in a single protocol-agnostic service exposed whenever
    2 TCP and UDP services on the same port were configured.
    
    Co-authored-by: Nate Sweet <nathanjsweet@pm.me>
    Co-authored-by: Antonio Ojea <aojea@google.com>
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -4,6 +4,7 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"strconv"
@@ -32,6 +33,7 @@ import (
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/service/healthserver"
+	"github.com/cilium/cilium/pkg/u8proto"
 )
 
 const anyPort = "*"
@@ -821,7 +823,7 @@ func (s *Service) UpdateBackendsState(backends []*lb.Backend) error {
 		}
 		be.State = updatedB.State
 		be.Preferred = updatedB.Preferred
-
+	nextService:
 		for id, info := range s.svcByID {
 			var p *datapathTypes.UpsertServiceParams
 			for i, b := range info.backends {
@@ -836,10 +838,17 @@ func (s *Service) UpdateBackendsState(backends []*lb.Backend) error {
 				found := false
 
 				if p, found = updateSvcs[id]; !found {
+					proto, err := u8proto.ParseProtocol(info.frontend.L4Addr.Protocol)
+					if err != nil {
+						errs = errors.Join(errs, fmt.Errorf("failed to parse service protocol for frontend %+v: %w", info.frontend, err))
+						continue nextService
+					}
+
 					p = &datapathTypes.UpsertServiceParams{
 						ID:                        uint16(id),
 						IP:                        info.frontend.L3n4Addr.AddrCluster.AsNetIP(),
 						Port:                      info.frontend.L3n4Addr.L4Addr.Port,
+						Protocol:                  byte(proto),
 						PrevBackendsCount:         len(info.backends),
 						IPv6:                      info.frontend.IsIPv6(),
 						Type:                      info.svcType,
@@ -1360,11 +1369,16 @@ func (s *Service) upsertServiceIntoLBMaps(svc *svcInfo, isExtLocal, isIntLocal b
 		}
 	}
 	svc.svcNatPolicy = natPolicy
+	protocol, err := u8proto.ParseProtocol(svc.frontend.L3n4Addr.L4Addr.Protocol)
+	if err != nil {
+		return err
+	}
 
 	p := &datapathTypes.UpsertServiceParams{
 		ID:                        uint16(svc.frontend.ID),
 		IP:                        svc.frontend.L3n4Addr.AddrCluster.AsNetIP(),
 		Port:                      svc.frontend.L3n4Addr.L4Addr.Port,
+		Protocol:                  uint8(protocol),
 		PreferredBackends:         preferredBackends,
 		ActiveBackends:            activeBackends,
 		NonActiveBackends:         nonActiveBackends,
