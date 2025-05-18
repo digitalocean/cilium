92e03daac81e6193e750245e03f9d1e11a08e1d3
Jordan Rife <jrife@google.com>
2025-05-17 19:15:40 -0700

    bpf: fib: Use BPF_FIB_LOOKUP_SKIP_NEIGH when available
    Use BPF_FIB_LOOKUP_SKIP_NEIGH in cases where the system supports it and
    we want to defer to bpf_redirect_neigh for neighbor resolution.
    
    Fixes: cilium#34503
    Signed-off-by: Jordan Rife <jrife@google.com>

--- pkg/datapath/bpf/gen.go
+++ pkg/datapath/bpf/gen.go
@@ -4,5 +4,4 @@
 // Package bpf provides Go skeletons containing BPF programs.
 package bpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go SockTerm ../../../bpf/bpf_sock_term.c
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go Probes ../../../bpf/bpf_probes.c
